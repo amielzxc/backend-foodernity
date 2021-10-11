3d586f2aa5cdcea1ff7ce7a8171e64636f798d19
unknown <amielzxc088@gmail.com>
2021-10-11 16:33:58 +0800

    new commit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Hello from the other side");
+  res.send("Hello from Stack Over Flowers");
 });
 
 const signup = require("./routes/signup.js");
