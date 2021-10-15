d56c6ac3071d7670e158b95a80766f44433c38cd
unknown <amielzxc088@gmail.com>
2021-10-15 19:09:00 +0800

    commit

--- server.js
+++ server.js
@@ -2,19 +2,20 @@ const express = require("express");
 const app = express();
 // const http = require("http");
 // const server = http.createServer(app);
-const port = process.env.PORT;
 var cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
-require("dotenv").config();
 
 app.get("/", (req, res) => {
   res.send("Hello from Stack Over Flowers");
 });
 
 const signup = require("./routes/signup.js");
+const { options } = require("./routes/signup.js");
 // const listingItem = require("./routes/listingItem.js");
 // const user = require("./routes/user.js");
 // const message = require("./routes/message.js");
