6dc8c1acf5b5f5b6220010cadaf859ef8811b4bd
unknown <amielzxc088@gmail.com>
2021-10-15 22:17:37 +0800

    testing backend and db

--- configDB.js
+++ configDB.js
@@ -2,12 +2,29 @@ const e = require("express");
 const mysql = require("mysql");
 require("dotenv").config();
 
-const db = mysql.createConnection({
-  user: process.env.DB_USER,
+// const db = mysql.createConnection({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DATABASE_NAME,
+// });
+
+var self = this;
+
+var db = mysql.createPool({
+  connectionLimit: 100,
+  waitForConnections: true,
+  queueLimit: 0,
   host: process.env.DB_HOST,
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE_NAME,
+  debug: true,
+  wait_timeout: 28800,
+  connect_timeout: 10,
 });
+
+self.configureExpress(pool);
 
 module.exports = db;
 
