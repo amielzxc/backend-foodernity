1b34d4d14f3857c27190ea9fa0e0cb0a3bdcd751
unknown <amielzxc088@gmail.com>
2021-10-18 23:59:02 +0800

    changed to fullName db

--- controllers/login.js
+++ controllers/login.js
@@ -51,86 +51,82 @@ const loginViaGoogle = (req, res) => {
   const changePasscode = "";
 
   //check if there is an existing email
-  db.query(
-    "SELECT loginMethod FROM user WHERE email=?",
-    [email],
-    (err, result) => {
-      if (err) {
-        console.log(err);
-        res.status(500).send(err);
-      } else {
-        if (result.length == 1) {
-          //check the login method if it matches the google login
-          console.log(result);
-          if (result == "google") {
-            console.log("Logged in");
-            res.send("Logged in");
-          } else {
-            console.log("Email is in use in different login method");
-            res.send("Email is in use in different login method");
-          }
-          // console.log(result[0]);
-          // db.query(
-          //   "SELECT * FROM users WHERE email= ? AND password= ?",
-          //   [email, password],
-          //   (err, result1) => {
-          //     if (err) {
-          //       console.log(err);
-          //       res.status(500).send(err);
-          //     } else {
-          //       if (result1.length == 1) {
-          //         console.log(result1);
-          //         console.log("logged in");
-          //         res
-          //           .status(200)
-          //           .send({ message: "logged in", accDetails: result1[0] });
-          //       } else {
-          //         res
-          //           .status(200)
-          //           .send({ message: "Wrong email/password!", accDetails: null });
-          //       }
-          //     }
-          //   }
-          // );
+  db.query("SELECT * FROM user WHERE email=?", [email], (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    } else {
+      if (result.length == 1) {
+        //check the login method if it matches the google login
+        console.log(result[0]);
+        if (result[0].loginMethod == "google") {
+          console.log("Logged in");
+          res.send("Logged in");
         } else {
-          // res.status(200).send("No account matched!");
-
-          //sign up the account first and save to the DB
-          db.query(
-            "INSERT INTO user(email, password, fullName, dateOfReg, loginMethod, userType, userStatus, changePasswordCode) VALUES(?,?,?,?,?,?,?,?)",
-            [
-              email,
-              password,
-              fullName,
-              dateOfReg,
-              loginMethod,
-              userType,
-              userStatus,
-              changePasscode,
-            ],
-            (err, result) => {
-              if (err) {
-                console.log(result);
-                console.log("error");
-                res.send(err);
-              } else {
-                console.log(result);
-                console.log({
-                  message: "new user added successfully via google login",
-                });
-                //
-                res.send({
-                  message: "new user added successfully via google login",
-                });
-
-                //after successfully saving to the database
-              }
-            }
-          );
+          console.log("Email is in use in different login method");
+          res.send("Email is in use in different login method");
         }
+        // console.log(result[0]);
+        // db.query(
+        //   "SELECT * FROM users WHERE email= ? AND password= ?",
+        //   [email, password],
+        //   (err, result1) => {
+        //     if (err) {
+        //       console.log(err);
+        //       res.status(500).send(err);
+        //     } else {
+        //       if (result1.length == 1) {
+        //         console.log(result1);
+        //         console.log("logged in");
+        //         res
+        //           .status(200)
+        //           .send({ message: "logged in", accDetails: result1[0] });
+        //       } else {
+        //         res
+        //           .status(200)
+        //           .send({ message: "Wrong email/password!", accDetails: null });
+        //       }
+        //     }
+        //   }
+        // );
+      } else {
+        // res.status(200).send("No account matched!");
+
+        //sign up the account first and save to the DB
+        db.query(
+          "INSERT INTO user(email, password, fullName, dateOfReg, loginMethod, userType, userStatus, changePasswordCode) VALUES(?,?,?,?,?,?,?,?)",
+          [
+            email,
+            password,
+            fullName,
+            dateOfReg,
+            loginMethod,
+            userType,
+            userStatus,
+            changePasscode,
+          ],
+          (err, result) => {
+            if (err) {
+              console.log(result);
+              console.log("error");
+              res.send(err);
+            } else {
+              console.log(result);
+              console.log({
+                message: "new user added successfully via google login",
+              });
+              //
+              res.send({
+                message: "new user added successfully via google login",
+              });
+
+              //after successfully saving to the database
+            }
+          }
+        );
       }
     }
-  );
+  });
 };
 
 module.exports = {
