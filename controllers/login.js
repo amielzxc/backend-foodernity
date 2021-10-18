940b3033838d531d40a75a2dd8b57c7baa892b27
unknown <amielzxc088@gmail.com>
2021-10-18 23:35:45 +0800

    changed to fullName db

--- controllers/login.js
+++ controllers/login.js
@@ -58,6 +58,7 @@ const loginViaGoogle = (req, res) => {
     } else {
       if (result.length == 1) {
         //check the login method if it matches the google login
+        console.log(result.loginMethod);
         if (result.loginMethod == "google") {
           console.log("Logged in");
           res.send("Logged in");
