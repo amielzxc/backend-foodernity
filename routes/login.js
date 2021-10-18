d5cecf56af240521ee4ab194ed5700346f598ee7
unknown <amielzxc088@gmail.com>
2021-10-18 23:08:36 +0800

    changed to fullName db

--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const { loginUser } = require("../controllers/login.js");
+const { loginUser, loginViaGoogle } = require("../controllers/login.js");
 const router = express.Router();
 
 router.post("/login", loginUser);
-
+router.post("/googleLogin", loginViaGoogle);
 module.exports = router;
