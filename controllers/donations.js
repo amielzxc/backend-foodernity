c199e9b15b5a169de728d674decbc961e3663731
unknown <amielzxc088@gmail.com>
2021-10-27 16:09:43 +0800

    call for donations

--- controllers/donations.js
+++ controllers/donations.js
@@ -141,8 +141,127 @@ const receiveDonation = (req, res) => {
     }
   );
 };
+
+const addCallForDonations = (req, res) => {
+  // const email = req.body.email;
+  // const password = req.body.password;
+  const title = req.body.title;
+  const description = req.body.description;
+  const imgPath = req.body.imgPath;
+  const status = req.body.status;
+
+  db.query(
+    "INSERT INTO callfordonation (title,description,imgPath,status)VALUES(?,?,?,?)",
+    [title, description, imgPath, status],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send(err);
+
+        // db.query(
+        //   "SELECT * FROM user WHERE email= ? AND password= ? AND userType='admin'",
+        //   [email, password],
+        //   (err, result1) => {
+        //     if (err) {
+        //       console.log(err);
+        //       res.status(500).send(err);
+        //     } else {
+        //       if (result1.length == 1) {
+        //         console.log(result1[0]);mber
+        //         console.log("logged in");
+        //         res.status(200).send(result1[0]);
+        //       } else {
+        //         res.status(200).send("Wrong email/password.");
+        //       }
+        //     }
+        //   }
+        // );
+      } else {
+        console.log(result);
+        console.log("added call for donation");
+        res.status(200).send(result);
+      }
+    }
+  );
+};
+
+const getCallForDonations = (req, res) => {
+  db.query(
+    "SELECT * FROM callfordonation WHERE status='active'",
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send(err);
+
+        // db.query(
+        //   "SELECT * FROM user WHERE email= ? AND password= ? AND userType='admin'",
+        //   [email, password],
+        //   (err, result1) => {
+        //     if (err) {
+        //       console.log(err);
+        //       res.status(500).send(err);
+        //     } else {
+        //       if (result1.length == 1) {
+        //         console.log(result1[0]);mber
+        //         console.log("logged in");
+        //         res.status(200).send(result1[0]);
+        //       } else {
+        //         res.status(200).send("Wrong email/password.");
+        //       }
+        //     }
+        //   }
+        // );
+      } else {
+        console.log(result);
+        console.log("fetch call for donation");
+        res.status(200).send(result);
+      }
+    }
+  );
+};
+
+const updateCallForDonations = (req, res) => {
+  const ID = req.body.ID;
+  db.query(
+    "UPDATE callfordonation SET status='fulfilled' WHERE callForDonationID=?",
+    [ID],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send(err);
+
+        // db.query(
+        //   "SELECT * FROM user WHERE email= ? AND password= ? AND userType='admin'",
+        //   [email, password],
+        //   (err, result1) => {
+        //     if (err) {
+        //       console.log(err);
+        //       res.status(500).send(err);
+        //     } else {
+        //       if (result1.length == 1) {
+        //         console.log(result1[0]);mber
+        //         console.log("logged in");
+        //         res.status(200).send(result1[0]);
+        //       } else {
+        //         res.status(200).send("Wrong email/password.");
+        //       }
+        //     }
+        //   }
+        // );
+      } else {
+        console.log(result);
+        console.log("update call for donation");
+        res.status(200).send(result);
+      }
+    }
+  );
+};
+
 module.exports = {
   getDonations,
   acceptDonation,
   receiveDonation,
+  addCallForDonations,
+  getCallForDonations,
+  updateCallForDonations,
 };
--- routes/donations.js
+++ routes/donations.js
@@ -3,11 +3,17 @@ const {
   getDonations,
   acceptDonation,
   receiveDonation,
+  addCallForDonations,
+  getCallForDonations,
+  updateCallForDonations,
 } = require("../controllers/donations.js");
 const router = express.Router();
 
 router.post("/getDonations", getDonations);
 router.post("/acceptDonations", acceptDonation);
 router.post("/receiveDonations", receiveDonation);
+router.post("/addCallForDonations", addCallForDonations);
+router.post("/getCallForDonations", getCallForDonations);
+router.post("/updateCallForDonations", updateCallForDonations);
 
 module.exports = router;
