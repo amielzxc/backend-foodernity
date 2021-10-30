b61f6c94971a24d120fa08ad005d525c334599e0
unknown <amielzxc088@gmail.com>
2021-10-30 13:30:50 +0800

    forgot pass func

--- controllers/forgotPassword.js
+++ controllers/forgotPassword.js
@@ -1,8 +1,9 @@
 const db = require("../configDB.js");
+const nodemailer = require("nodemailer");
 
 const getChangePasswordCode = (req, res) => {
   const email = req.body.email;
-
+  let changePassCode = Math.floor(Math.random() * 888888 + 111111);
   db.query("SELECT * FROM user WHERE email=?", [email], (err, result) => {
     if (err) {
       console.log(err);
@@ -10,7 +11,30 @@ const getChangePasswordCode = (req, res) => {
     } else {
       if (result.length == 1) {
         console.log(result[0]);
-        res.send(result[0].changePasswordCode);
+
+        var mail = nodemailer.createTransport({
+          service: "gmail",
+          auth: {
+            user: "foodernity2021@gmail.com",
+            pass: "foodernityApp2021",
+          },
+        });
+        var mailOptions = {
+          from: "foodernity2021@gmail.com",
+          to: email,
+          subject: "Foodernity Verification Code",
+          text: "The code for change password is " + changePassCode,
+        };
+
+        mail.sendMail(mailOptions, function (error, info) {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log("Email sent: " + info.response);
+          }
+        });
+
+        res.send(changePassCode);
       } else {
         res.status(200).send("No existing account.");
       }
