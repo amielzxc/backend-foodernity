22fbc09e60f0c708601d19ebb0d45889b1f2325f
unknown <amielzxc088@gmail.com>
2021-11-11 00:58:55 +0800

    none

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Hello from Stack Over Flowers");
+  res.send("Sira apk ng capstone");
 });
 
 const signup = require("./routes/signup.js");
